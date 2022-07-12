f632b256940a861ffcacaa720734d6227a4f202f
kurdock <iainlim853@gmail.com>
2022-07-13 04:18:38 +0800

    Added searchpypi.py for opening first 5 search results on pypi.org

---
+++ searchpypi.py
@@ -0,0 +1,34 @@
+#
+#
+#
+#
+#
+
+from re import L
+import sys, requests, webbrowser, bs4
+
+print('Searching...')
+
+res = requests.get('https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+links = bs4.BeautifulSoup(res.text, 'html.parser')
+
+openLinks = links.select('.package-snippet')
+
+numOpen = min(5, len(links))
+
+# Range starts at 0 by default.
+# Note that the 'href' attribute value in the returned elements do not
+# have the initial https://pypi.org part, so that part to be concatenated
+# to the href attribute value.
+
+for i in range(numOpen): 
+    urlToOpen = 'https://pypi.org' + openLinks[i].get('href')
+    print('Opening ' + urlToOpen)
+    webbrowser.open(urlToOpen)
+
+
+
+
+
